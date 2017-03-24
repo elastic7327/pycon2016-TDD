12bfb5d1d42369b69e3a6e21713942f595fe5382
elastic7327 <elastic7327@gamil.commit>
2017-03-24 16:31:13 +0800

    GOAT

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -12,11 +12,41 @@ class NewVersinTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrive_it_later(self):
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
         self.browser.get("http://localhost:8000")
+
+        # She notices the apge title and hearder mention to-do lists
         self.assertIn("To-Do", self.browser.title)
+        heard_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', heard_text)
 
+        # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+                inputbox.get_attribute('placeholder'),
+                'Enter a to-do item'
+        )
 
+        # She types "Buy peacock feathers" into a text box (Edith's hobby)
+        # is typing fly-fishing lures)
 
+        inputbox.send_keys("Buy peacock feathers")
+        inputbox.send_keys("\n")
+
+        # When  she hits enter , the page updates, and noew the page lists
+        # "1 : Buy peacock feathers" as an item in a to-di list table
+
+        table = self.browser.find_element_by_id("id_lists_table")
+        rows = table.find_elements_by_tag_name('tr')
+
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+
+        # There is still a text box inviting her to add another item. she
+        # enters "Use peacock feathers to make a fly" (Edith is very
+        # methodical)
+
+        self.fail("Finish the test!! hoooray!!")
 
 
 if __name__ == "__main__":
