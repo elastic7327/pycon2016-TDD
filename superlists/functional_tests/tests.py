ac3741a8df64fa2de70f81e634995d41e316e175
Daniel KIM <jhe7327@naver.com>
2017-03-25 01:47:51 +0800

    hola

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -1,8 +1,10 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
-class NewVersinTest(unittest.TestCase):
+class NewVersinTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Chrome()
@@ -19,7 +21,8 @@ class NewVersinTest(unittest.TestCase):
     def test_can_start_a_list_and_retrive_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get("http://localhost:8000")
+        # self.browser.get("http://localhost:8000")
+        self.browser.get(self.live_server_url)
 
         # She notices the apge title and hearder mention to-do lists
         self.assertIn("To-Do", self.browser.title)
