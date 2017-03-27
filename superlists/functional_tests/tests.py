e793aaea24047e458f932988aabe2b8db9b17f8d
daniel <elastic7327@gmail.com>
2017-03-28 01:08:58 +0800

    add on

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -1,4 +1,4 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
@@ -6,7 +6,7 @@ import unittest
 import ipdb as br
 
 
-class NewVersinTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Chrome()
@@ -66,35 +66,60 @@ class NewVersinTest(LiveServerTestCase):
         # We use a new browser session to make sure that no information
         # of Edith's is comming through from cookies etc#
 
-        br.set_trace()
-
-        self.browser.quit()
-        self.browser = webdriver.Chrome()
+#        self.browser.quit()
+#        self.browser = webdriver.Chrome()
 
         # Franchis visites the home page. There is no sign of Edith's
         # list
 
+#        self.browser.get(self.live_server_url)
+#        page_text = self.browser.find_element_by_tag_name('body').text
+#        self.assertNotIn('Buy peacock feathers', page_text)
+#        self.assertNotIn('make a fly', page_text)
+#
+#        # Fancis starts a new list by entering a new item. He
+#        # is less interesting than Edith . . . .
+#
+#        inputbox = self.browser.find_element_by_id('id_new_item')
+#        inputbox.send_keys('Buy milk')
+#        inputbox.send_keys('\n')
+#
+#        # Francis gets his own unique URL
+#        francis_list_url = self.browser.current_url
+#        self.assertRegexpMatches(francis_list_url, '/lists/.+')
+#        self.assertNotEqual(francis_list_url, edith_list_url)
+#
+#        # Again, there is no trace of Edith's list
+#        page_text = self.browser.find_element_by_tag_name('body').text
+#        self.assertNotIn('Buy peacock feathers', page_text)
+#        self.assertIn('Buy milk', page_text)
+
+    def test_layout_and_styling(self):
+        # Edith goes to the home page
         self.browser.get(self.live_server_url)
-        page_text = self.browser.find_element_by_tag_name('body').text
-        self.assertNotIn('Buy peacock feathers', page_text)
-        self.assertNotIn('make a fly', page_text)
+        self.browser.set_window_size(1024, 768)
 
-        # Fancis starts a new list by entering a new item. He
-        # is less interesting than Edith . . . .
-
+        # She notices the input box is nicely centered
         inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('Buy milk')
-        inputbox.send_keys('\n')
+        self.assertAlmostEqual(
+                inputbox.location['x'] + inputbox.size['width'] / 2,
+                512,
+                delta=5
+        )
 
-        # Francis gets his own unique URL
-        francis_list_url = self.browser.current_url
-        self.assertRegexpMatches(francis_list_url, '/lists/.+')
-        self.assertNotEqual(francis_list_url, edith_list_url)
+        # She starts a new list and sees the input is nicely
+        # centered there too..
 
-        # Again, there is no trace of Edith's list
-        page_text = self.browser.find_element_by_tag_name('body').text
-        self.assertNotIn('Buy peacock feathers', page_text)
-        self.assertIn('Buy milk', page_text)
+        inputbox.send_keys('testing\n')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+                inputbox.location['x'] + inputbox.size['width'] / 2,
+                512,
+                delta=5
+        )
+
+
+
 
         def test_is_over(self):
             self.fail("Finish the test!! hoooray!!")
--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -4,12 +4,7 @@ from lists.models import Item, List
 
 
 def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/')
-
-    items = Item.objects.all()
-    return render(request, 'lists/home.html', {'items': items})
+    return render(request, 'lists/home.html')
 
 
 def view_list(request, list_id):
